2f94a5da4e46d0d85e5a74ced1152fe3dee71bb7
gustavo reyes <gustavino182@gmail.com>
2024-01-25 21:07:52 -0500

    cambios nuevos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const BASE_API_URL = "https://backend-task-voice.azurewebsites.net/api";
-export const BASE_URL = "https://backend-task-voice.azurewebsites.net";
+export const BASE_API_URL = "https://backend-tesis-prod.onrender.com/api";
+export const BASE_URL = "https://backend-tesis-prod.onrender.com/";
 
 //export const BASE_API_URL = "https://demo-task-voice.onrender.com/api";
 //export const BASE_URL = "https://demo-task-voice.onrender.com";
